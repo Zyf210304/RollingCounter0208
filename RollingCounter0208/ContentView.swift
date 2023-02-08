913ee19fbba77dd914c84f2b559bf1177601ba44
张亚飞 <672233580@qq.com>
2023-02-08 13:38:54 +0800

    Initial Commit

---
+++ RollingCounter0208/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RollingCounter0208
+//
+//  Created by 张亚飞 on 2023/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RollingCounter0208/RollingCounter0208App.swift
@@ -0,0 +1,17 @@
+//
+//  RollingCounter0208App.swift
+//  RollingCounter0208
+//
+//  Created by 张亚飞 on 2023/2/8.
+//
+
+import SwiftUI
+
+@main
+struct RollingCounter0208App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
